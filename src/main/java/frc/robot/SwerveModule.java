d40551adce9f064a5c5795adf69b0da1712ea253
bert-team7036 <123598742+bert-team7036@users.noreply.github.com>
2023-03-09 14:48:16 -0500

    Update SwerveModule.java

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
@@ -118,7 +114,6 @@ public class SwerveModule {
 		m_drivingMotor.setVoltage(driveOutput + driveFeedforward);
 		m_turningMotor.setVoltage(turnOutput + turnFeedforward);
 	}
-
 	public double getDistance(){
 		return m_drivingEncoder.getPosition();
 	}
