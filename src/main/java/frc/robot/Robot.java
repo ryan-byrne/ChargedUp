446c322732e5f580354f8502e909a76bc099e7af
bert-team7036 <123598742+bert-team7036@users.noreply.github.com>
2023-03-09 14:50:28 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
@@ -16,8 +12,8 @@ public class Robot extends TimedRobot {
 	private final Arm m_arm = new Arm(18, 19, 20, 9);//CANId for Arm motors
 	private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
-	// Slew rate limiters to make joystick input more smooth
-	//Imposes a rate limit on how quickly the robot can change speed or direction
+	
+	//Imposes a rate limit on how quickly the robot can change speed or direction, to make joystick input more smooth
 	private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
 	private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
 	private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
