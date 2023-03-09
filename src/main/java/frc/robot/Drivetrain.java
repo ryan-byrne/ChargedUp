f19eeed3842b28515554c063a94cb141d8a019bb
bert-team7036 <123598742+bert-team7036@users.noreply.github.com>
2023-03-09 14:27:12 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+// DriveTrain handles control and movement of swerveDrive
+public class Drivetrain {
+		//initialize instance variables
+	public static final double kMaxSpeed = 3.0; 
+	public static final double kMaxAngularSpeed = Math.PI; 
+
+//What is this for?
+	private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+	private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+	private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+	private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
+
+	//Four swerveModule objects, one for each wheel of the robot
+	// Module A
+	private final SwerveModule m_frontLeft = new SwerveModule("A",10, 11);
+	// Module B
+	private final SwerveModule m_frontRight = new SwerveModule("B", 12, 13);
+	// Module C
+	private final SwerveModule m_backLeft = new SwerveModule("C", 14, 15);
+	// Module D
+	private final SwerveModule m_backRight = new SwerveModule("D", 16, 17);
+	//Robot's orientation in space
+	private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+	//Calculates wheel speeds and directions
+	private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+			m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+	//tracks robot's position and orientation on field 
+	private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+			m_kinematics,
+			m_gyro.getRotation2d(),
+			new SwerveModulePosition[] {
+					m_frontLeft.getPosition(),
+					m_frontRight.getPosition(),
+					m_backLeft.getPosition(),
+					m_backRight.getPosition()
+			});
+		//constructor 
+	public Drivetrain() {
+		m_gyro.reset();
+	}
+
+	/**
+	 * Method to drive the robot using joystick info.
+	 *
+	 * @param xSpeed        Speed of the robot in the x direction (forward).
+	 * @param ySpeed        Speed of the robot in the y direction (sideways).
+	 * @param rot           Angular rate of the robot.
+	 * @param fieldRelative Whether the provided x and y speeds are relative to the
+	 *                      field.
+	 */
+	public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+		var swerveModuleStates = m_kinematics.toSwerveModuleStates(
+				fieldRelative
+						? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+						: new ChassisSpeeds(xSpeed, ySpeed, rot));
+		SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
+		m_frontLeft.setDesiredState(swerveModuleStates[0]);
+		m_frontRight.setDesiredState(swerveModuleStates[1]);
+		m_backLeft.setDesiredState(swerveModuleStates[2]);
+		m_backRight.setDesiredState(swerveModuleStates[3]);
+		
+	}
+
+	/** Updates the field relative position of the robot. */
+	public void updateOdometry() {
+		m_odometry.update(
+				m_gyro.getRotation2d(),
+				new SwerveModulePosition[] {
+						m_frontLeft.getPosition(),
+						m_frontRight.getPosition(),
+						m_backLeft.getPosition(),
+						m_backRight.getPosition()
+				});
+	}
+}
